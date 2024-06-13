b9f115d5c81a418f9e35145eaf1e1f29233be838
Assaf <1214assaf@gmail.com>
2024-06-13 14:49:37 +0300

    feat: Add NotFoundPage component for handling missing pages

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 export default function MealsSlugPage({ params }) {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
